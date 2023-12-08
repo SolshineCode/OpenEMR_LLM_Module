639a91ed2410850f738755523c841fb486b7b2e2
SolshineCode <143902425+SolshineCode@users.noreply.github.com>
2023-12-08 11:01:55 -0800

    Update llm.py
    Assured model is downloaded locally

--- llm.py
+++ llm.py
@@ -1,20 +1,23 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import DownloadManager
 
-# Configurable model name
-MODEL_NAME = "gpt2" 
+# Configurable model name - Replace "gpt2" with the name of your desired model in quotes
+MODEL_NAME = "gpt2"
+
+# Download model and tokenizer locally
+download_manager = DownloadManager(cache_dir="~/.cache/huggingface")
+model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, local_files_only=True, download_manager=download_manager)
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, local_files_only=True, download_manager=download_manager)
 
 def generate_response(prompt, patient_data=None):
-  try:
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-    model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
+    try:
+        formatted_prompt = f"{prompt} {patient_data}" if patient_data else prompt
 
-    formatted_prompt = f"{prompt} {patient_data}" if patient_data else prompt
+        inputs = tokenizer.encode(formatted_prompt, return_tensors="pt")
+        outputs = model.generate(inputs, max_length=150, num_return_sequences=1)
 
-    inputs = tokenizer.encode(formatted_prompt, return_tensors="pt")
-    outputs = model.generate(inputs, max_length=150, num_return_sequences=1)
-
-    response = tokenizer.decode(outputs[0])
-    return response
-  except Exception as e:
-    print(f"Error generating response: {e}")
-    return "Error: Could not generate response."
+        response = tokenizer.decode(outputs[0])
+        return response
+    except Exception as e:
+        print(f"Error generating response: {e}")
+        return "Error: Could not generate response."
