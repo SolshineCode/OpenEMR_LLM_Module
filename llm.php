bcad73bbc2cf8a0d4498c29636d4c535f68545ee
SolshineCode <143902425+SolshineCode@users.noreply.github.com>
2023-12-07 20:09:34 -0800

    Create llm.php

---
+++ llm.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Medical Assistant LLM</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script>
+    $(document).ready(function(){
+      $("#submit").click(function(){
+        var prompt = $("#prompt").val();
+        // Fetch patient data if available
+        var patient_data = ""; // Implement logic to retrieve patient data
+        $.post("response.php", {prompt: prompt, patient_data: patient_data}, function(data){
+          $("#response").html(data);
+        });
+      });
+      $("#feedback").click(function(){
+        var feedback = $("#feedback_text").val();
+        // Implement logic to send feedback to server
+        alert("Feedback submitted. Thank you!");
+      });
+    });
+  </script>
+</head>
+<body>
+  <h1>Medical Assistant LLM</h1>
+  <textarea id="prompt" placeholder="Enter your prompt here"></textarea>
+  <button id="submit">Submit</button>
+  <hr>
+  <h3>Response</h3>
+  <div id="response"></div>
+  <hr>
+  <h3>Feedback</h3>
+  <textarea id="feedback_text" placeholder="Provide feedback on the response"></textarea>
+  <button id="feedback">Submit Feedback</button>
+</body>
+</html>
