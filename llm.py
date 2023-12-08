06be63443c500fd3871ea5ffe98e59c2dc84a1df
SolshineCode <143902425+SolshineCode@users.noreply.github.com>
2023-12-08 11:38:59 -0800

    Update llm.py
    Added Flask to improve python and php integration while keeping data local

--- llm.py
+++ llm.py
@@ -1,23 +1,25 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from transformers import DownloadManager
+from flask import Flask, request, jsonify
+from transformers import AutoModelForCausalLM, AutoTokenizer, DownloadManager
 
-# Configurable model name - Replace "gpt2" with the name of your desired model in quotes
+app = Flask(__name__)
+
 MODEL_NAME = "gpt2"
-
-# Download model and tokenizer locally
 download_manager = DownloadManager(cache_dir="~/.cache/huggingface")
-model = AutoModelForCausalLM.from_pretrained(MODEL_NAME, local_files_only=True, download_manager=download_manager)
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, local_files_only=True, download_manager=download_manager)
+model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
-def generate_response(prompt, patient_data=None):
-    try:
-        formatted_prompt = f"{prompt} {patient_data}" if patient_data else prompt
+@app.route('/generate', methods=['POST'])
+def generate():
+    data = request.get_json()
+    prompt = data['prompt']
+    patient_data = data.get('patient_data')
 
-        inputs = tokenizer.encode(formatted_prompt, return_tensors="pt")
-        outputs = model.generate(inputs, max_length=150, num_return_sequences=1)
+    formatted_prompt = f"{prompt} {patient_data}" if patient_data else prompt
+    inputs = tokenizer.encode(formatted_prompt, return_tensors="pt")
+    outputs = model.generate(inputs, max_length=150, num_return_sequences=1)
+    response = tokenizer.decode(outputs[0])
 
-        response = tokenizer.decode(outputs[0])
-        return response
-    except Exception as e:
-        print(f"Error generating response: {e}")
-        return "Error: Could not generate response."
+    return jsonify({'response': response})
+
+if __name__ == '__main__':
+    app.run(port=5000)
